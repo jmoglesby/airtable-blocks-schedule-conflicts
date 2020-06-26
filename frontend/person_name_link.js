66201d33145c6f3524867a3336f8d7a0e1fe1766
Jeremy Oglesby <iaminaband@gmail.com>
2020-06-25 21:49:35 -0700

    Add styled-components

--- frontend/person_name_link.js
+++ frontend/person_name_link.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Heading, Text } from '@airtable/blocks/ui';
+import { Heading, Text, Box } from '@airtable/blocks/ui';
 
 function PersonNameLink(props) {
   return (
-    <div>
+    <Box borderRadius="large">
       <Heading marginBottom={-1}>{props.person.getCellValue("Name")}</Heading>
       <Text textColor="light" fontWeight={300} paddingLeft={3}>conflicts:</Text>
-    </div>
+    </Box>
   );
 }
 
